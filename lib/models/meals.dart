9046474da754b02011be130bb15fa89bd510352c
domel06 <58642541+domel06@users.noreply.github.com>
2021-10-23 19:09:19 +0300

    adding detail screen to display steps and ingredients

--- lib/models/meals.dart
+++ lib/models/meals.dart
@@ -9,7 +9,7 @@ class Meal {
   final List<String> categories;
   final String title;
   final String imageUrl;
-  final List<String> ingridients;
+  final List<String> ingredients;
   final List<String> steps;
   final int duration;
   final Complexity complexity;
@@ -24,7 +24,7 @@ class Meal {
       @required this.categories,
       @required this.title,
       @required this.imageUrl,
-      @required this.ingridients,
+      @required this.ingredients,
       @required this.steps,
       @required this.duration,
       @required this.complexity,
@@ -32,5 +32,5 @@ class Meal {
       @required this.isGlutenFree,
       @required this.isLactoseFree,
       @required this.isVegan,
-      @required this.isVegetarian, List<String> ingredients});
+      @required this.isVegetarian,});
 }
